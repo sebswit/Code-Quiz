576c20eec248b93974f885a5bba70ab8ce8b4efb
sebswit <sebastian.switakowski@gmail.com>
2023-12-03 11:27:05 +0000

    added switcher for difficulty level

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,7 +1,9 @@
 // create starting const eg:
 // 
 // const start button 
-// const questions => document.querySelector??
+const startButton = document.getElementById("start");
+// const questionsBeginner => document.querySelector??
+// const questionsExpert => document.querrySelectorAll??
 // const answers => document.querySelector??
 // const initials => ??
 
@@ -9,11 +11,21 @@
 // let score = 0;
 // let timer
 
-// startQuiz => function using button, timer, 
-// showQuestion => function with loop
+// Level Typo
+levelSwitcher.addEventListener("click", function() {
+    if (level === "beginner") {
+      level = "beginner";
+      quizContainer = easyContainer;
+    } else {
+      level = "expert";
+      quizContainer = hardContainer;
+    }
+  });
+// startQuiz => function using button, timer, and radiobutton for level
+// showQuestion => function with loop based on radiobutton
 // checkAnswer => function with if else
 //                  correct - adds points (add sound)
 //                  incorrect - substract points, time penalty (add sound)
 // endQuiz => function displays score, resets timer
 // saveScore => file score.js function with initials input(max 3 letters) store it in localStorage
-//
+// window.location.replace(url:"scores.js")
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,10 +1,24 @@
 // create a question container with object
-// const = quizContainer = ......
-// const questions =[
+// const = quizContainerBeginer = ......
+// const = quizContainerExpert = .....
+// const questionsBeginer =[
 //  {
 //    questionTitle =          HTML question-title
 //    question:"",             
 //    answers:["","","",""],   HTML choices
 //    correctIndex: ,
 //}
+// const questionsExpert =[
+//  {
+//    questionTitle =          HTML question-title
+//    question:"",             
+//    answers:["","","",""],   HTML choices
+//    correctIndex: ,
+//}
+
+
+
+
+
+
 // window.replace url = highscore.html
