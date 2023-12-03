2bc8fc83978b9659b57b66dc231fee87a3eb49ba
sebswit <sebastian.switakowski@gmail.com>
2023-12-03 18:26:07 +0000

    removed switch, added update timer and const linked with html

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,26 +1,38 @@
-// create starting const eg:
-// 
 // const start button 
 const startButton = document.getElementById("start");
 // const questionsBeginner => document.querySelector??
-// const questionsExpert => document.querrySelectorAll??
-// const answers => document.querySelector??
-// const initials => ??
+const questionTitle = document.getElementById("question-title");
+// Define your answers
+const choices = document.getElementById("choices");
+// const initials
+const initialsInput = document.getElementById("initials");
+//const timer
+const myTimer = document.getElementById("Timer");
+//const questions
+const questionDiv = document.getElementById("questions");
+//const time
+const time = document.getElementById("time");
+// const endscreen
+const endScreenDiv = document.getElementById("end-screen");
 
-// let currentQuestion = 0;
-// let score = 0;
-// let timer
+let currentQuestion = 0;
+let score = 0;
+let timer = 0;
+let index = 0;
+let level = "beginner";
+let quizContainer = easyContainer;
+let count = quizContainer.length *15;
+let answer = quizContainer[1];
 
-// Level Typo
-levelSwitcher.addEventListener("click", function() {
-    if (level === "beginner") {
-      level = "beginner";
-      quizContainer = easyContainer;
-    } else {
-      level = "expert";
-      quizContainer = hardContainer;
-    }
-  });
+// Timer Function
+function updateTimer() {
+  time.textContent = count;
+count--;
+ if (count === 0) {
+  alert("Time is up");
+  endQuiz();
+}
+}
 // startQuiz => function using button, timer, and radiobutton for level
 // showQuestion => function with loop based on radiobutton
 // checkAnswer => function with if else
